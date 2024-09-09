fbab9cf24441aeaad6541a18fc4c94fb3efc100a
Nícolas Michielon <61835002+nicolasmichielon@users.noreply.github.com>
2024-09-09 23:05:11 +0000

    Fix fix class Professor

--- associacao.py
+++ associacao.py
@@ -1,15 +1,3 @@
-class Professor():
-    def __init__(self, nome, endereco, orientando):
-        self.__nome = nome
-        self.__endereco = endereco
-        self.__orientando = orientando
-
-    @property
-    def orientando(self):
-        return self.__orientando.nome
-       
-
-
 class Aluno():
     def __init__(self, nome, endereco):
         self.__nome = nome
@@ -20,7 +8,26 @@ class Aluno():
         return self.__nome
 
 
+class Professor():
+    def __init__(self, nome, endereco, orientando: Aluno):
+        if(isinstance(orientando, Aluno)):
+            self.__orientando = orientando
+        else:
+            self.__orientando = None
+            print("Aluno incorreto.")
+        self.__nome = nome
+        self.__endereco = endereco
+
+    @property
+    def orientando(self):
+        if(isinstance(self.__orientando, Aluno)):
+            return self.__orientando.nome
+        return False
+
+
 nick2 = Aluno("Nicolas Michielon", "Florianopolis")
 nick = Professor("Nicholas Derham", "Florianopolis", nick2)
+nick3 = Professor("Nicholas Derham", "Florianopolis", nick)
 
 print(nick.orientando)
+print(nick3.orientando)
